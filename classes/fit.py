3189f88bdbf235b67a8e65b1e57f3fee73487f71
Hadrian Montes <hadrianmontes@gmail.com>
2016-05-18 22:26:47 +0200

    Started fit class

---
+++ classes/fit.py
@@ -0,0 +1,8 @@
+class fit:
+    def __init__(self,listdata,datafile_index,dataset_index,index):
+        self.listdata=listdata
+        self.datafile_index=datafile_index
+        self.dataset_index=dataset_index
+        self.index=index
+        
+        
