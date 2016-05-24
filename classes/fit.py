a05e6fe36ce5a7de3d1225c469c398f8a69001eb
Hadrian Montes <hadrianmontes@gmail.com>
2016-05-24 23:28:59 +0200

    Added some minor work, changed the scheme

--- classes/fit.py
+++ classes/fit.py
@@ -1,8 +1,13 @@
+from scipy.optimize import curve_fit
+# Import the functions to use for the fits
+import functions.functions as functions
+
+# Define the fit class
 class fit:
     def __init__(self,listdata,datafile_index,dataset_index,index):
         self.listdata=listdata
         self.datafile_index=datafile_index
         self.dataset_index=dataset_index
         self.index=index
-        
-        
+        self.load_fits()
+
